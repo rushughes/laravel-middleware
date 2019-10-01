efb31cb82adf840571e45552265c23f05b469f75
Rus Hughes <russell.hughes@gmail.com>
2019-10-01 17:23:45 +0200

    s23 l160

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -15,7 +15,6 @@ class RoleMiddleware
      */
     public function handle($request, Closure $next)
     {
-        return redirect('/');
-        //return $next($request);
+        return $next($request);
     }
 }
