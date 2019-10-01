28e566591800311b7b99313b9deba1f4df5ac46d
Rus Hughes <russell.hughes@gmail.com>
2019-10-01 17:20:54 +0200

    s23 l159

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin/user/roles', ['middleware' => 'role', function () {
+  return "Middleware role";
+}]);
